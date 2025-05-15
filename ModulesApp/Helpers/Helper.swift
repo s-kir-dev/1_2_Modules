174f4d4a04798ddefd783169bb8d106d01bb2013
Kirill <syskirill.com>
2025-05-15 11:01:21 +0300

    screens were setted up

---
+++ ModulesApp/Helpers/Helper.swift
@@ -0,0 +1,12 @@
+//
+//  Helper.swift
+//  ModulesApp
+//
+//  Created by Кирилл Сысоев on 15.05.2025.
+//
+
+import Foundation
+import UIKit
+
+let storyboard = UIStoryboard(name: "Main", bundle: nil)
+let authVC = storyboard.instantiateViewController(withIdentifier: "AuthViewController") as! AuthViewController
