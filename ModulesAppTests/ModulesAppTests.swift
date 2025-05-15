b7771f3a014d3b3b2c8224c794195653719c0dde
Kirill <syskirill.com>
2025-05-15 10:32:45 +0300

    Initial Commit

---
+++ ModulesAppTests/ModulesAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ModulesAppTests.swift
+//  ModulesAppTests
+//
+//  Created by Кирилл Сысоев on 15.05.2025.
+//
+
+import Testing
+@testable import ModulesApp
+
+struct ModulesAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
